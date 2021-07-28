8005a7a58ffcd57245ff2c0f1c5cb4f167054783
Krishnakanth <krishnakanth.pps@gmail.com>
2021-07-28 14:54:01 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ const formEvent = form.addEventListener("submit", (event) => {
 });
 
 const sendMail = (mail) => {
-  fetch("https://nodemailer-vic-lo.herokuapp.com/send", {
+  fetch("https://emailer123.herokuapp.com/send", {
     method: "post",
     body: mail,
   }).then((response) => {
